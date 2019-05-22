bc0376cc60f62469ce13bba9744c63fdf8b69057
pipdeveloper <pipdevs@gmail.com>
2019-05-22 07:41:40 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MessageDistributionHttpServiceV1_1 = require("./MessageDistributionHttpServiceV1");
-exports.MessageDistributionHttpServiceV1 = MessageDistributionHttpServiceV1_1.MessageDistributionHttpServiceV1;
-//# sourceMappingURL=index.js.map
