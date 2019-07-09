2cc0e986cb962a4aeb19316dfc10bcd6b1e9b08a
pipdeveloper <pipdevs@gmail.com>
2019-07-09 05:32:37 +0000

    project build by Travis CI [skip ci]

--- obj/src/data/version1/RecipientV1.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class RecipientV1 {
-    id?: string;
-    name?: string;
-    email?: string;
-    phone?: string;
-    language?: string;
-}
--- obj/src/logic/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { IMessageDistributionController } from './IMessageDistributionController';
-export { MessageDistributionController } from './MessageDistributionController';
